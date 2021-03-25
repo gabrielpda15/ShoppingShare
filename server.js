1a4092368f58094b72617262219162525065179a
Gabriel Pupim <gabriel.pda15@gmail.com>
2021-03-25 00:19:44 -0300

    Ajustes server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/ngShoopingShare`));
+app.use(express.static(`$/dist/ngShoopingShare`));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/dist/ngShoopingShare/index.html`));
+  res.sendFile(path.join(`/dist/ngShoopingShare/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
