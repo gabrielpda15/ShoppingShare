febf6e0abb0d63cd70a898284c603bdeaf423df2
Gabriel Pupim <gabriel.pda15@gmail.com>
2021-03-24 22:20:24 -0300

    Ajuste diretório de saida

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/ngRandomFeeders`));
+app.use(express.static(`${__dirname}/dist/ngShoopingShare`));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/dist/ngRandomFeeders/index.html`));
+  res.sendFile(path.join(`${__dirname}/dist/ngShoopingShare/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
